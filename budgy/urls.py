e2ad6e36cd147d1e0775b2060ece3972244c5e58
jasehackman <jasehackman@gmail.com>
2019-03-05 15:35:48 -0600

    added url for registration

--- budgy/urls.py
+++ budgy/urls.py
@@ -15,4 +15,5 @@ router.register(r'expenses', views.ExpenseViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('register', views.register_user, name='register_user')
 ]
