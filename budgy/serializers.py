ff8cbe97d779cd1a0a23d418759f1c13c9280ccf
jasehackman <jasehackman@gmail.com>
2019-03-20 11:20:19 -0500

    Document classes

--- budgy/serializers.py
+++ budgy/serializers.py
@@ -4,26 +4,36 @@ from django.contrib.auth.models import User
 
 # Todo: refactor for search
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    """ Displays User info
+
+    """
 
     class Meta:
         model = User
         fields = "__all__"
 
 class BudgetSerializer(serializers.HyperlinkedModelSerializer):
+    """ Displays budget information.
 
 
+    """
     class Meta:
         model = Budget
         fields = ('name', 'amount', 'spent', 'remaining', 'id', 'user', 'percent', 'archived', 'allocated', 'to_allocate')
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
-    # budget=BudgetSerializer(read_only=True)
+    """ Displays category information.
+
+    """
     budget_id=serializers.PrimaryKeyRelatedField(queryset=Budget.objects.all(), source='budget')
     class Meta:
         model = Category
         fields = ('name', 'amount', 'budget', 'spent', 'remaining', 'id', 'budget_id', "url", 'percent')
 
 class ExpenseSerializer(serializers.HyperlinkedModelSerializer):
+    """ Displays expense information.
+
+    """
     category_id=serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), source='category')
 
     class Meta:
