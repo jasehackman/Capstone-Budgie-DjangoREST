8c2f339e5aa1c365df06032970466630768812ea
jasehackman <jasehackman@gmail.com>
2019-03-21 12:08:45 -0500

    add sql file for data demo

---
+++ dataFeed.sql
@@ -0,0 +1,12 @@
+INSERT INTO budgy_budget VALUES (25, 'Trip to Ashville', 500, 0, 2);
+
+INSERT INTO budgy_category VALUES (44, 'Hotel', 200, 25);
+INSERT INTO budgy_category VALUES (45, 'Gas', 100, 25);
+INSERT INTO budgy_category VALUES (46, 'Food', 150, 25);
+INSERT INTO budgy_category VALUES (47, 'Shopping', 50, 25);
+
+INSERT INTO budgy_expense VALUES (42, 'Country Inn and Suites', 150, "2019-03-20", "2 days at $75 a day", 44);
+INSERT INTO budgy_expense VALUES (43, 'Shell', 34, "2019-03-20", "", 45);
+INSERT INTO budgy_expense VALUES (44, 'Shell', 23, "2019-03-20", "", 45);
+INSERT INTO budgy_expense VALUES (45, 'PIZZA!!!', 23, "2019-03-20", "It was amazing!!", 46);
+INSERT INTO budgy_expense VALUES (46, 'Coffee', 4.74, "2019-03-20", "Burnt :(", 46);
